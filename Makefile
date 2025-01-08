a8e1440e66136728c018329402cc608f7a31f5b9
Vic K <kondratskayavictoria@gmail.com>
2025-01-08 23:27:41 +0200

    CHORE: created db, established connectivity, added commands to Makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
 run:
 	poetry run python backend/manage.py runserver
+
+create-su:
+	poetry run python backend/manage.py createsuperuser
+
+make-migrations:
+	poetry run python backend/manage.py makemigrations
+
+migrate:
+	poetry run python backend/manage.py migrate
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -69,8 +69,12 @@ class Dev(Configuration):
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'MST',
+            'USER': os.environ["DB_USER"],
+            'PASSWORD': os.environ["DB_PASSWORD"],
+            'HOST': os.environ["DB_HOST"],
+            'PORT': os.environ["DB_PORT"],
         }
     }
 
